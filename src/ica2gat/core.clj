26681baaf343f68ea92dc1de936f716ee72a800a
Tom Hicks <hickst@tohono.com>
2011-12-20 00:16:15 -0700

    Add function to make adjacency matrix.

--- src/ica2gat/core.clj
+++ src/ica2gat/core.clj
@@ -14,6 +14,10 @@
   (filter (fn [[n1 n2]] (not= n1 n2))
           (comb/selections aSet 2)))
 
+(defn make-adjacency-matrix [nodes pairs]
+  (let [freq-map (frequencies pairs)]
+    (ica2gat.adjmat.AdjacencyMatrix. nodes freq-map)))
+
 (defn write-pairs [pairs]
   (println "Source,Target,Type")
   (doseq [p pairs]
@@ -24,7 +28,7 @@
          components (read-components filename)
          nodes (sort (apply set/union components))
          pairs (mapcat make-pairs components) ]
-    (ica2gat.adjmat.AdjacencyMatrix. nodes {})
+    (make-adjacency-matrix nodes pairs)
     ;; (write-pairs pairs)
   )
 )
