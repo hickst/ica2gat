c73995936d81d8a72add6770b8d7edb580c5037c
Tom Hicks <hickst@tohono.com>
2011-12-20 18:58:10 -0700

    Refactor creation function to adjmat file. Add method to output matrix.

--- src/ica2gat/adjmat.clj
+++ src/ica2gat/adjmat.clj
@@ -1,12 +1,13 @@
 (ns ica2gat.adjmat
+  (:require [clojure.math.combinatorics :as comb])
+  (:require [clojure.set :as set])
   (:gen-class)
 )
 
 (defprotocol AdjacencyMatrixProtocol
-  "Defines a limited set of operations for Adjacency Matrices: reading in
-   pairs of nodes representing edges, returning vectors of node pairs from
-   the upper and/or lower triangles of the adjacency matrix, and returning
-   the matrix itself."
+  "Defines a limited set of operations for Adjacency Matrices:
+   returning vectors of node pairs from the upper and/or lower triangles
+   of the adjacency matrix, writing the matrix, and returning the matrix."
 
   (lower-triangle [amat]
     "Return a sequence of node pairs from the lower triangle of the adjacency matrix"
@@ -21,6 +22,10 @@
      Each sequence represents a row in the adjacency matrix, with the first
      row being the node names (row and column labels).
      Example: for a 3x3 matrix [[:A :B :C][1 2 3][4 5 6][7 8 9]]"
+  )
+
+  (write-matrix [amat]
+    "Write the adjacency matrix to the current value of *out* in CSV form."
   )
 )
 
@@ -39,4 +44,25 @@
         (partition amat-dim
           (for [k1 node-names k2 node-names]
             (get weights (seq [k1 k2]) 0))))))
+
+  (write-matrix [amat]
+    (doseq [row (.matrix amat)]
+      (let [cols (interpose ", " row)]
+        (doseq [col cols] (print col))
+        (println))))
 )
+
+
+(defn make-pairs [nodes]
+  "Return a sequence of all possible pairs of nodes, except pairs
+   containing the same node twice."
+  (filter (fn [[n1 n2]] (not= n1 n2))
+          (comb/selections nodes 2)))
+
+(defn make-adjacency-matrix [components]
+  "Create and return an adjacency matrix from the given sequence
+   of components (sets of node names)."
+  (let [ nodes (sort (apply set/union components))
+         pairs (mapcat make-pairs components)
+         freq-map (frequencies pairs) ]
+    (ica2gat.adjmat.AdjacencyMatrix. nodes freq-map)))
--- src/ica2gat/core.clj
+++ src/ica2gat/core.clj
@@ -1,22 +1,8 @@
 (ns ica2gat.core
-  (:require [clojure.math.combinatorics :as comb])
   (:require [clojure.java.io :as io])
-  (:require [clojure.set :as set])
   (:require [ica2gat.adjmat])
   (:gen-class)
 )
-
-(defn make-adjacency-matrix [nodes pairs]
-  "Create and return an adjacency matrix from the given sequences of
-   node names and node pairings."
-  (let [freq-map (frequencies pairs)]
-    (ica2gat.adjmat.AdjacencyMatrix. nodes freq-map)))
-
-(defn make-pairs [nodes]
-  "Return a sequence of all possible pairs of nodes, except pairs
-   containing the same node twice."
-  (filter (fn [[n1 n2]] (not= n1 n2))
-          (comb/selections nodes 2)))
 
 (defn read-components [filename]
   "Read the ICA components, as a Clojure data structure, directly from
@@ -26,19 +12,19 @@
   (with-open [rdr (java.io.PushbackReader. (io/reader filename))]
     (read rdr)))
 
-(defn write-pairs [pairs]
-  (println "Source,Target,Type")
-  (doseq [p pairs]
-    (println (str (first p) "," (second p) ",Undirected"))))
-
 (defn -main [ & args]
   (let [ filename (first args)
          components (read-components filename)
-         nodes (sort (apply set/union components))
-         pairs (mapcat make-pairs components) ]
-    (make-adjacency-matrix nodes pairs)
-    ;; (write-pairs pairs)
+         amat (ica2gat.adjmat/make-adjacency-matrix components) ]
+    (.write-matrix amat)
+    ;; (.write-pairs amat)
+    amat
   )
 )
 
 ;; (-main "resources/sample-input-file")
+
+;; (defn write-pairs [amat]
+;;   (println "Source,Target,Type")
+;;   (doseq [p (.node-names amat)]
+;;     (println (str (first p) "," (second p) ",Undirected"))))
